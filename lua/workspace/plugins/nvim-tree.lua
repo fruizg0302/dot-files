3c8bf4862c4c90a0fe98102e85e740d3322e0ccb
Fernando Ruiz Guzman <fruizg0302@me.com>
2022-11-02 18:07:20 -0600

    updating nvim-tree config

--- lua/workspace/plugins/nvim-tree.lua
+++ lua/workspace/plugins/nvim-tree.lua
@@ -14,6 +14,17 @@ vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
 -- configure nvim-tree
 nvimtree.setup({
 	-- change folder arrow icons
+	view = {
+		adaptive_size = true,
+		centralize_selection = true,
+		width = 30,
+		hide_root_folder = false,
+		side = "left",
+		preserve_window_proportions = false,
+		number = false,
+		relativenumber = false,
+		signcolumn = "yes",
+	},
 	renderer = {
 		icons = {
 			glyphs = {
@@ -21,16 +32,47 @@ nvimtree.setup({
 					arrow_closed = "", -- arrow when folder is closed
 					arrow_open = "", -- arrow when folder is open
 				},
+				highlight_opened_files = "all",
+				git = {
+					unstaged = "✗",
+					staged = "✓",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "★",
+					deleted = "",
+					ignored = "◌",
+				},
 			},
 		},
 	},
 	-- disable window_picker for
 	-- explorer to work well with
 	-- window splits
+	git = {
+		enable = true,
+		ignore = true,
+		show_on_dirs = true,
+		timeout = 400,
+	},
+	filesystem_watchers = {
+		enable = true,
+		debounce_delay = 50,
+	},
 	actions = {
 		open_file = {
+			quit_on_open = false,
+			resize_window = true,
 			window_picker = {
 				enable = false,
+			},
+		},
+		file_popup = {
+			open_win_config = {
+				col = 1,
+				row = 1,
+				relative = "cursor",
+				border = "shadow",
+				style = "minimal",
 			},
 		},
 	},
