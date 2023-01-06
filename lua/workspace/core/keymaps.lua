645bcc210d08f92add6e353464fac7e54118647e
Fernando Ruiz <fruizg0302@me.com>
2023-01-06 10:42:27 -0600

    Updating keymapping and terminal usage

--- lua/workspace/core/keymaps.lua
+++ lua/workspace/core/keymaps.lua
@@ -62,3 +62,6 @@ keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current c
 
 -- restart lsp server (not on youtube nvim video)
 keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
+
+keymap.set("t", "<esc>", [[<C-\><C-n>]])
+keymap.set("t", "jk", [[<C-\><C-n>]])
