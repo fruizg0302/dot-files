b1601ceef6e77872a0617024e01084516a42ae8f
fernandoruiz <fernando.ruiz@hey.com>
2023-07-06 08:38:25 -0600

    Updating theme

--- lua/workspace/core/colorscheme.lua
+++ lua/workspace/core/colorscheme.lua
@@ -1,6 +1,52 @@
-local status, n = pcall(require, "rose-pine")
-if (not status) then return end
+local status, n = pcall(require, "catppuccin")
+if not status then
+	return
+end
 
+n.setup({
+	flavour = "mocha", -- latte, frappe, macchiato, mocha
+	background = { -- :h background
+		light = "latte",
+		dark = "mocha",
+	},
+	transparent_background = true, -- disables setting the background color.
+	show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+	term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+	dim_inactive = {
+		enabled = false, -- dims the background color of inactive window
+		shade = "dark",
+		percentage = 0.15, -- percentage of the shade to apply to the inactive window
+	},
+	no_italic = false, -- Force no italic
+	no_bold = false, -- Force no bold
+	no_underline = false, -- Force no underline
+	styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+		comments = { "italic" }, -- Change the style of comments
+		conditionals = { "italic" },
+		loops = {},
+		functions = {},
+		keywords = {},
+		strings = {},
+		variables = {},
+		numbers = {},
+		booleans = {},
+		properties = {},
+		types = {},
+		operators = {},
+	},
+	color_overrides = {},
+	custom_highlights = {},
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		nvimtree = true,
+		telescope = true,
+		notify = false,
+		mini = false,
+		-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+	},
+})
+--[[
 n.setup({
 	--- @usage 'auto'|'main'|'moon'|'dawn'
 	variant = 'auto',
@@ -50,8 +96,9 @@ n.setup({
 		StatusLine = { fg = 'love', bg = 'love', blend = 10 },
 	}
 })
+--]]
 
-vim.cmd [[colorscheme rose-pine]]
+vim.cmd([[colorscheme catppuccin]])
 --configurations for solarized theme
 --local cb = require('colorbuddy.init')
 --local Color = cb.Color
